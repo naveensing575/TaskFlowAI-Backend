62112615a1aa3f6568543507dab503d6c41adc96
naveensing575 <naveensing575@gmail.com>
2025-06-30 20:48:09 +0530

    chore: add morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,19 @@ import express from 'express'
 import authRoutes from './routes/authRoutes'
 import taskRoutes from './routes/taskRoutes'
 import { errorHandler } from './middlewares/errorMiddleware'
+import morgan from 'morgan';
 
 const app = express()
 
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+}
 
 app.use('/api/auth', authRoutes)
 app.use('/api/task', taskRoutes)
