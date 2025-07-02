e8eecb3e43c0a0f784af8af6cef3b34148877291
naveensing575 <naveensing575@gmail.com>
2025-07-02 13:26:53 +0530

    feat: add getMe controller and secure /me route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { protect } from './../middlewares/authMiddleware'
 import { Router } from 'express'
-import { loginUser, registerUser } from '../controllers/authController'
+import { loginUser, registerUser, getMe } from '../controllers/authController'
 
 const router = Router()
 
 router.post('/register', registerUser)
 router.post('/login', protect, loginUser)
+router.get("/me", protect, getMe);
 
 export default router
