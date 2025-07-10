f2e91460d16589e1128eff083774f86d12ea9b34
naveensing575 <naveensing575@gmail.com>
2025-07-10 13:18:18 +0530

    feat(admin): add admin activity log

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -3,6 +3,7 @@ import {
   getAllUsers,
   updateUserRole,
   deleteUser,
+  getAllActivityLogs
 } from '../controllers/adminController'
 
 import { protect } from '../middlewares/authMiddleware'
@@ -13,5 +14,8 @@ const router = express.Router()
 router.get('/', protect, authorizeRoles('admin'), getAllUsers)
 router.patch('/:id/role', protect, authorizeRoles('admin'), updateUserRole)
 router.delete('/:id', protect, authorizeRoles('admin'), deleteUser)
+
+// Get all activity logs
+router.get('/activity', protect, authorizeRoles('admin'), getAllActivityLogs)
 
 export default router
